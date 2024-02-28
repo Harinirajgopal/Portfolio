9b6836e25dcb524003d142cb8f0fc23590f428b7
Harinirajgopal <149541714+Harinirajgopal@users.noreply.github.com>
2024-02-29 03:31:39 +0530

    portfolio
    portfolio

---
+++ src/App.js
@@ -0,0 +1,139 @@
+
+
+import React from 'react';
+import Img1 from './Images/Harini.jpg'
+
+
+function CV() {
+  return (
+    <div className="cv">
+      <div className="header">
+        <h1><b>Harini R</b></h1>
+        <img src={Img1} width="150px" height="100px"/>
+        
+        <p>Intern as Full-stack Developer</p>
+        <p><b>GTyde Technologies Pvt Ltd, Bangalore, Karnataka, India</b></p>
+      </div>
+      
+      <div className="about-me">
+        <h2><u>About Me:</u></h2>
+        <p>I have 6 months of experience working in Frontend Development. Currently, I am working as an Intern as a Full-stack Developer at GTyde Technologies Pvt Ltd, where I develop functionalities for web applications and building web products for clients. I’m deeply passionate about creating loved and trusted products that help people navigate their careers and achieve their career goals.</p>
+      </div>
+      <div className="work-experience">
+        <h2><b><u>Work Experience:</u></b></h2>
+        <p><b>Front End Developer | GTyde Technologies Pvt Ltd (Intern)</b></p>
+        <p>UdayNagar, Tinfactory, Bangalore</p>
+        <ul>
+          <li>Develop Front End and back end Functionalities to improve responsiveness and overall Performance.</li>
+          <li>Integrate User-facing elements into applications.</li>
+          <li>Co-ordinate with internal teams to understand user requirements and provide technical Solutions.</li>
+          <li>Build table frames and forms to write script within the browser to enhance site functionality.</li>
+        </ul>
+        <p><b>Analyst - ANZ Support Services, 2016 - 2019</b></p>
+        <p>Manyata Tech Park, Hebbal, Bangalore</p>
+        <ul>
+          <li>Remediation of Mortgage information into the updated Website.</li>
+          <li>Discharge of existing mortgages and lending loans for the purchase of new properties.</li>
+        </ul>
+        <p><b>Accounts Associate Level-2 - First American India, 2014 - 2016</b></p>
+        <p>ITPL Whitefield, Bangalore</p>
+        <ul>
+          <li>Processing PO and Non PO invoices.</li>
+          <li>Research and Resolve invoice discrepancies and issues.</li>
+          <li>Audited invoices for quality checking.</li>
+          <li>Monitoring TAT and error to maintain quality level in the team.</li>
+          <li>Conducting daily hurdle and clear team members queries.</li>
+        </ul>
+      </div>
+      <div className="projects">
+        <h2><b><u>Projects:</u></b></h2>
+        <p>Projects worked during Internship (June 2023 - Nov 2023) at GTyde Technology Pvt Ltd</p>
+        <p><b>Project 1: E-commerce Application</b></p>
+        <p>An e-commerce project is an online platform that allows customers to purchase products or services through the internet. It is the virtual space where you showcase products, and online customers make selections. Your website acts as the product shelves, sales staff, and cash register of your online business channel.</p>
+        <p><b>Project 2: Weather Application</b></p>
+        <p>Weather App is the application of science and technology to predict the conditions of the atmosphere for a given location and time. It’s a way of predicting things like cloud cover, rain, snow, wind speed, and temperature before they happen.</p>
+      </div>
+      <div className="education">
+        <h2><b><u>Education:</u></b></h2>
+        <ul>
+            <li>Management in Business Administration (M.B.A-2013),VTU University, Belguam, Karnataka</li>
+            <li>Bachelor of Computer Application (B.C.A), University of Bangalore</li>
+            <li>Govt PU College for Girls-2008 , Pre-university Board of Karnataka</li>
+            <li>IBM English Medium High School-2006 ,Passed out, Bangarpet, Kolar, Karnataka</li>
+        </ul>
+       
+        <div className="languages">
+        <h2><b><u>Languages:</u></b></h2>
+        <p>English, Kannada, Telugu, Tamil, Hindi</p>
+      </div>
+      <div className="tech-skills">
+        <h2><u><b>Technical Skills:</b></u></h2>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Bootstrap</li>
+          <li>Javascript</li>
+          <li>React JS</li>
+          <li>Python</li>
+          <li>Django Framework</li>
+          <li>MySQL</li>
+          <li>Web development</li>
+        </ul>
+      </div>
+      <div className="hard-skills">
+        <h2><b><u>Hard Skills:</u></b></h2>
+        <ul>
+          <li>Project Management</li>
+          <li>Microsoft Excel (VLOOKUP, HLOOKUP, PIVOT Table)</li>
+          <li>Problem Solving</li>
+          <li>Coding</li>
+          <li>People Management</li>
+        </ul>
+      </div>
+      </div>
+      <div className="interests">
+        <h2><b><u>Interests:</u></b></h2>
+        <ul>
+          <li>Travelling</li>
+          <li>Cooking</li>
+          <li>Listening to Music</li>
+          <li>Dancing</li>
+          <li>Reading Books</li>
+        </ul>
+      </div>
+      <div className="achievements">
+        <h2><b><u>Achievements:</u></b></h2>
+        <ul>
+          <li>I have been awarded "Best Performer" in the respective months in 2015 (June - July).</li>
+          <li>Have been awarded "Rocking Eagle Awards" in maintaining 100 percent accuracy.</li>
+          <li>Have actively participated in testing Oracle for the new line of business as an end-user.</li>
+          <li>New idea implementation for process improvements.</li>
+        </ul>
+      </div>
+      <div className="strengths">
+        <h2><b><u>Strengths:</u></b></h2>
+        <ul>
+          <li>Ability to grasp new concepts quickly.</li>
+          <li>Dedicated towards work.</li>
+          <li>Strong determination that motivates me to success.</li>
+          <li>Good knowledge in arithmetic and statistic fields that ensure accounting calculation.</li>
+        </ul>
+      </div>
+      <div className="references">
+        <h2><u>References:</u></h2>
+        <p>Mr. Raghav - <a href="mailto:rraghav23c@gmail.com">rraghav23c@gmail.com</a></p>
+        <p>Mr. Sudharshan S. - <a href="mailto:sudharsant026@gmail.com">sudharsant026@gmail.com</a></p>
+      </div>
+      <div className="contact">
+        <h2><u>Contact:</u></h2>
+        <p><b>Address:</b>  170, Bazzar Street, Udaynagar, Bangarpet-560016</p>
+        <p><b> MobileNo:</b>  +91 9740449930</p>
+        <p>Email: <a href="mailto:Harinirajgopal1991@gmail.com">Harinirajgopal1991@gmail.com</a></p>
+        <p>LinkedIn: <a href="http://linkedin.com/in/harini-rajgopal-215a9a125">Harini Rajgopal</a></p>
+        <p>GitHub: <a href="https://www.github.com/Harinirajgopal">Harini Rajgopal</a></p>
+      </div>
+    </div>
+  );
+}
+
+export default CV;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import CV from "./App"
+import React from "react"
+import  ReactDOM  from "react-dom"
+
+ReactDOM.render(
+    <div>
+        <CV/>
+    </div>,document.getElementById("root")
+)
